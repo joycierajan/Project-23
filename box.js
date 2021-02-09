43aa7e5a9e736cdf1c0c72f6d9b63fa3914e4879
joycierajan <74923023+joycierajan@users.noreply.github.com>
2021-02-09 16:27:18 +0000

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box{
+
+    constructor(x,y,width,height,color){
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        World.add(world,this.body);
+
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    display() {
+        var pos = this.body.position
+        fill(this.color)
+        rectMode(CENTER)
+        rect(pos.x, pos.y, this.width, this.height)
+    }
+
+}
